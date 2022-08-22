32852b5a61e4b25c5755634a8bd02d6e1b2b262c
Tarun <tarun.mittal@bravvox.com>
2022-08-22 11:06:28 +0530

    fixed updated vehicle query

--- src/controllers/vehicles.js
+++ src/controllers/vehicles.js
@@ -1,7 +1,7 @@
 const methods = {};
 const { validationResult } = require("express-validator");
 const { queryInstance } = require("../db/connection");
-const { dateDifference, currentDate } = require("..//helpers");
+const { currentDate } = require("../helpers");
 
 methods.getVehicles = async (req, res) => {
   try {
@@ -15,18 +15,13 @@ methods.getVehicles = async (req, res) => {
 
 methods.updateVehicles = async (req, res) => {
   try {
-    const { vehicle_id, to_dept_id } = req.body;
-    const vehicle = await queryInstance(`SELECT * from vehicles WHERE vehicle_id = '${vehicle_id}'`);
-    const from_dept_id = vehicle[0].dept_id;
-    const days = dateDifference(vehicle[0].date_in);
+    const { vehicle_id, from_dept_id, to_dept_id, days } = req.body;
     await queryInstance(`UPDATE vehicles SET dept_id = '${to_dept_id}', date_in = '${currentDate}' WHERE vehicle_id = '${vehicle_id}'`);
     await queryInstance(`INSERT INTO summary (vehicle_id, from_dept_id, to_dept_id, days) VALUES ('${vehicle_id}', '${from_dept_id}', '${to_dept_id}', '${days}') RETURNING *`);
-    
     const existedCount = await queryInstance(`SELECT * from counts WHERE dept_id = '${from_dept_id}'`);
     if(existedCount && existedCount.length > 0) await queryInstance(`UPDATE counts SET days = '${Number(existedCount[0].days) + Number(days)}' WHERE dept_id = '${from_dept_id}'`);
     else await queryInstance(`INSERT INTO counts (dept_id, days) VALUES ('${from_dept_id}', '${days}') RETURNING *`);
-
-    res.json({ result: "Vehile updated successfully!" });
+    res.json({ result: "Vehicle updated successfully!" });
   } catch (err) {
     console.error(err.message);
     return res.status(500).send(err.message);
@@ -35,13 +30,10 @@ methods.updateVehicles = async (req, res) => {
 
 methods.addVehicle = async (req, res) => {
   const errors = validationResult(req);
-
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
-
   const { dept_id, stock, year, make, model, ucm_in, date_in } = req.body;
-
   try {
     const vehicles = await queryInstance(`INSERT INTO vehicles (dept_id, stock, year, make, model, ucm_in, date_in, variant, notes) VALUES ('${dept_id}', '${stock}', '${year}', '${make}', '${model}', '${ucm_in}', '${date_in}', 'Variant', 'Out with Driver') RETURNING *`);
     res.json({ vehicles });
