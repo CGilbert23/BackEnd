94b3224cc949aa44d22743b38d0892c9ed1348b3
Kapil Vashisht <kapil@webspero.com>
2022-09-29 20:48:28 +0530

    Frontline summary enabled.

--- src/controllers/summary.js
+++ src/controllers/summary.js
@@ -6,15 +6,29 @@ methods.getSummary = async (req, res) => {
 
     const counts = await queryInstance(`SELECT dept.dept_id, dept.name, (select array_agg(days) from summary where dept.dept_id = summary.dept_in_id AND summary.dept_out_id IS NOT NULL AND summary.date_out IS NOT NULL AND summary.date_out > current_timestamp - interval '30 day') AS days From departments AS dept`);
 
+    const frontLines = await queryInstance(`SELECT dept.dept_id, dept.name, (select array_agg(EXTRACT(DAY FROM summary.date_in::timestamp - vehicles.ucm_in::timestamp)) FROM vehicles JOIN summary ON summary.vehicle_id = vehicles.vehicle_id where dept.dept_id = summary.dept_in_id AND summary.dept_out_id IS NULL AND summary.date_out IS NULL AND summary.date_in > current_timestamp - interval '30 day') AS days From departments AS dept where name like 'Frontline%'`);
+
     const depts = await queryInstance('SELECT * from departments');
-    const customCounts = depts.map((ele) => {
+
+    var customCounts = depts.map((ele) => {
       const count = counts.find(e => e.dept_id === ele.dept_id);
       return {
         ...ele,
         days: count ? count.days : 0
       }
     })
-    res.json({ summary: customCounts.filter(e => !e.name.includes('Frontline')) });
+
+    customCounts = counts.map((ele) => {
+      const frontLine = frontLines.find(e => e.dept_id === ele.dept_id);
+      return {
+        ...ele,
+        ...frontLine
+      }
+    })
+
+    // res.json({ summary: customCounts.filter(e => !e.name.includes('Frontline')) });
+    res.json({ summary : customCounts });
+
   } catch (err) {
     console.error(err.message);
     return res.status(500).send(err.message);
